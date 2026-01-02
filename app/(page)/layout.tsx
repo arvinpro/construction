e291b4fd9d4c0c6f5284f0ac2d4853a6752f72af
Arbin Mahato <arma_csit2078@lict.edu.np>
2026-01-02 16:59:36 +0545

    page transition

--- app/(page)/layout.tsx
+++ app/(page)/layout.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
 import { Toaster } from "react-hot-toast";
+import { motion } from "framer-motion";
 
 export default function RootLayout({
   children,
@@ -12,8 +15,15 @@ export default function RootLayout({
       <head />
       <body className="bg-white min-h-screen">
         <Navbar />
-        {children}
-        <Toaster  position="top-right" reverseOrder={false}/>
+        <motion.div
+          initial={{ opacity: 0, x: 100 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: -100 }}
+          transition={{ duration: 0.5, ease: [0.4, 0, 0.2, 1] }}
+        >
+          {children}
+        </motion.div>
+        <Toaster position="top-right" reverseOrder={false} />
         <Footer />
       </body>
     </html>
